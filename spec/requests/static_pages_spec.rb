b7dcd518c340baf7e0383d3fe2d0b2bde5898d66
Zuzana Dostalova <sanna23@icloud.com>
2013-05-10 10:57:03 +0200

    Titles without layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,14 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
+
 
   describe "Help page" do
 
@@ -16,13 +23,26 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
+
 
   describe "About page" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
