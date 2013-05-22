6ada0bbed547cd6eb5985a2e07eb1b8cdd43529c
Zuzana Dostalova <sanna23@icloud.com>
2013-05-22 23:07:12 +0200

    Home page without custom title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,31 +3,34 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
 
   describe "Help page" do
-
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit '/static_pages/help'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+        :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -41,9 +44,9 @@ describe "Static pages" do
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit '/static_pages/about'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+        :text => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
