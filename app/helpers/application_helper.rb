1d0eda61557950fa3ad9bb72b1cd03119827b7b2
Zuzana Dostalova <sanna23@icloud.com>
2013-06-01 12:29:02 +0200

    Helper comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # Return the title based on if the custom title is provided
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
