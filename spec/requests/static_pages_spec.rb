c1e5354486a9e2a6740a225561329b2be750bba9
Zuzana Dostalova <sanna23@icloud.com>
2013-07-30 23:16:58 +0200

    rspec for sign up page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -53,9 +53,9 @@ describe "Static pages" do
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
     page.should have_selector 'title', text: full_title('')
-    # click_link "Sign up now!"
-    # page.should have_selector 'title', text: full_title('Sign up')
-    # click_link "sample app"
-    # page.should have_selector 'title', text: full_title('Sample app')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
   end
 end
